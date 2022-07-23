054d3dc8a0784c28bd31b03a74c57b6264f60eac
Davidyz <zcabzyu@ucl.ac.uk>
2022-07-23 12:31:13 +0100

    Make the password invisible.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from connector import Connector
 from course_pdf_downloader import CoursePDFDownloader
-
+import getpass
 
 if __name__ == "__main__":
-    username = input("Please enter your username:\n")
-    password = input("Please enter your password:\n")
+    username = input("Please enter your username: ")
+    password = getpass.getpass("Please enter your password: ")
 
     connector = Connector()
     connector.login(username, password)
