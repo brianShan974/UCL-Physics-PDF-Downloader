fc3e385ef7282cb5514f425f0fe1a7d13cf314f3
Davidyz <zcabzyu@ucl.ac.uk>
2022-07-23 12:21:00 +0100

    Use ReGex to match module code.

--- course_link_finder.py
+++ course_link_finder.py
@@ -1,20 +1,23 @@
 from connector import Connector
+import re
 
 
 class CourseLinkFinder:
     def __init__(self, connector: Connector):
         self.connector = connector
         soup = self.connector.post_login_soup
-        course_tags = [tag for tag in soup.find_all(
-            "a",
-            class_="list-group-item list-group-item-action"
-        ) if self.is_valid_course_name(tag.text)]
-        self.course_dict = {
-            tag.text[:8]: tag["href"] for tag in course_tags
-        }
+        course_tags = [
+            tag
+            for tag in soup.find_all(
+                "a", class_="list-group-item list-group-item-action"
+            )
+            if self.is_valid_course_name(tag.text)
+        ]
+        self.course_dict = {tag.text[:8]: tag["href"] for tag in course_tags}
 
     def is_valid_course_name(self, name: str):
-        return name[:4] == "PHAS" and name[4:8].isdigit()
+        course_pattern = re.compile("\w{4}\d{4}")
+        return course_pattern.match(name) is not None
 
     def get_course_link_by_course_id(self, course_id: str):
         return self.course_dict[course_id]
